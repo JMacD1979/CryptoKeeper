0a730b12254fee70357f72f09968bef24c07976e
Jason MacDonald <A4X8QZZ@usac.mmm.com>
2018-05-31 14:18:19 -0400

    add coin detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoinsComponent } from './coins/coins.component';
 import { FormsModule } from '@angular/forms';
+import { CoinDetailComponent } from './coin-detail/coin-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoinsComponent
+    CoinsComponent,
+    CoinDetailComponent
   ],
   imports: [
     BrowserModule,
